29b5acaec4acf7d709d31af22108d8e23ea97583
seheonha <seheonha@hanyang.ac.kr>
2025-02-16 23:03:23 +0900

    delete build & devel folder

--- build/bsw/communication/pcan_router/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/seheonha/git/AutoHYU-Control-Personality/src/bsw/communication/pcan_router/include".split(';') if "/home/seheonha/git/AutoHYU-Control-Personality/src/bsw/communication/pcan_router/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "pcan_router"
-PROJECT_SPACE_DIR = "/home/seheonha/git/AutoHYU-Control-Personality/devel"
-PROJECT_VERSION = "0.0.0"
--- src/lib/acados/build/interfaces/acados_c/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seheonha/git/AutoHYU-Control/src/lib/acados/interfaces/acados_c
-# Build directory: /home/seheonha/git/AutoHYU-Control/src/lib/acados/build/interfaces/acados_c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
