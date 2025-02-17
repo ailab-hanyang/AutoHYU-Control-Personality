75870239e3d196d0ca9a5c0942f9f20859c7763e
Personality <temp@email.com>
2025-02-17 22:32:53 +0900

    Clean git caches

--- src/lib/acados/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seheonha/git/AutoHYU-Control/src/lib/acados
-# Build directory: /home/seheonha/git/AutoHYU-Control/src/lib/acados/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("acados")
-subdirs("interfaces/acados_c")
-subdirs("external")
--- src/lib/acados/build/external/qpoases/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seheonha/git/AutoHYU-Control/src/lib/acados/external/qpoases
-# Build directory: /home/seheonha/git/AutoHYU-Control/src/lib/acados/build/external/qpoases
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
