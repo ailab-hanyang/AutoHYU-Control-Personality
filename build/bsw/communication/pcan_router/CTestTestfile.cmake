76a3bae153eb247f05dee152b5cf55566b95310e
Personality <temp@email.com>
2025-02-17 22:28:05 +0900

    Ignore devel and build directories

--- build/bsw/communication/pcan_router/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seheonha/git/AutoHYU-Control-Personality/src/bsw/communication/pcan_router
-# Build directory: /home/seheonha/git/AutoHYU-Control-Personality/build/bsw/communication/pcan_router
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
